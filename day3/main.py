dbe317e142b4307e0c263b47b440967651aaad4f
Nolan Khounborin <khounborinn@gmail.com>
2024-12-03 00:52:36 -0500

    Day 3 done

---
+++ day3/main.py
@@ -0,0 +1,42 @@
+import re
+
+def part1() -> int:
+    answer = 0
+
+    with open('input.txt', 'r') as file:
+        corruptedData = file.read()
+
+    mulList = re.findall("mul\(\d*,\d*\)", corruptedData)
+    for mul in mulList:
+        numbers = re.findall("\d+", mul)
+        x, y = [int(num) for num in numbers]
+        answer += (x*y)
+
+    return answer
+
+
+def part2() -> int:
+    with open('input.txt', 'r') as file:
+        corruptedData = file.read()
+    mulList = re.findall("do\(\)|mul\(\d*,\d*\)|don\'t\(\)", corruptedData)
+    answer = multiply(mulList)
+    return answer
+
+def multiply(mulList) -> int:
+    answer = 0
+    checkMultiply = True
+    for mul in mulList:
+        if mul == "do()":
+            checkMultiply = True
+        elif mul == "don't()":
+            checkMultiply = False
+        if checkMultiply and mul != "do()":
+            numbers = re.findall("\d+", mul)
+            x, y = [int(num) for num in numbers]
+            answer += (x*y)
+
+    return answer
+
+
+print(part1())
+print(part2())
